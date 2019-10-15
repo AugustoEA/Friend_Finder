ed96a3732f72c623e319402391134ab20fa794df
Augusto <gutoea@gmail.com>
2019-10-14 20:58:27 -0500

    Friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,147 @@
 var friends = [
+    {
+        "Name": "Predator",
+        "Photo": "https://cdn.vox-cdn.com/thumbor/i4yuOFwegxlzaTzOwPT6lcRlSFQ=/124x0:1474x900/1200x800/filters:focal(124x0:1474x900)/cdn.vox-cdn.com/uploads/chorus_image/image/48838179/predator.0.0.jpg",
+        "Score": [
+          2,
+          4,
+          1,
+          2,
+          2,
+          5,
+          3,
+          2,
+          3,
+          1
+        ]
+      },
+    {
+        "Name": "Major Alan 'Dutch' Schaefer",
+        "Photo": "https://townsquare.media/site/442/files/2013/08/Predator.jpg?w=980&q=75",
+        "Score": [
+          2,
+          4,
+          1,
+          2,
+          2,
+          5,
+          3,
+          2,
+          3,
+          1
+        ]
+      },
+      {
+        "name": "George Dillon",
+        "photo": "https://imgc.allpostersimages.com/img/print/u-g-PJ5FAH0.jpg?w=550&h=550&p=0",
+        "scores": [
+          2,
+          4,
+          1,
+          2,
+          4,
+          1,
+          2,
+          5,
+          3,
+          2
+        ]
+      },
+      {
+        "name": "Anna",
+        "photo": "https://www.avpgalaxy.net/wordpress/wp-content/uploads/2019/06/what-happened-to-anna-01-701x464.jpg",
+        "scores": [
+          4,
+          2,
+          1,
+          5,
+          5,
+          2,
+          3,
+          4,
+          5,
+          2
+        ]
+      },
+      {
+        "name": "Jorge 'Pancho' Ramirez",
+        "photo": "https://66.media.tumblr.com/fb29ff47f3caf626cd01c0d6af28e5eb/tumblr_pg8r79LVQb1svlz2ko1_1280.jpg",
+        "scores": [
+          2,
+          3,
+          4,
+          4,
+          1,
+          2,
+          2,
+          2,
+          3,
+          5
+        ]
+      },
+      {
+        "name": "Mac Eliot",
+        "photo": "https://i.pinimg.com/originals/48/73/38/487338b9451d2970e9425f0227447c8c.jpg",
+        "scores": [
+          1,
+          2,
+          3,
+          4,
+          5,
+          4,
+          3,
+          2,
+          1,
+          5
+        ]
+      },
+      {
+        "name": "Blain Cooper",
+        "photo": "https://vignette.wikia.nocookie.net/avp/images/7/7a/Blain.png/revision/latest?cb=20130413101849",
+        "scores": [
+          5,
+          4,
+          3,
+          2,
+          1,
+          2,
+          3,
+          4,
+          5,
+          4
+        ]
+      },
+      {
+        "name": "Billy Sole",
+        "photo": "http://www.coastaldigest.com/sites/default/files/Sonny.jpg",
+        "scores": [
+          3,
+          2,
+          1,
+          3,
+          2,
+          1,
+          3,
+          2,
+          1,
+          3
+        ]
+      },
+      {
+        "name": "Rick Hawkins",
+        "photo": "https://img.cinemablend.com/filter:scale/quill/5/9/3/5/c/7/5935c7087993aea0f56413c8efc1ad76d8a77ba0.jpg?mw=600",
+        "scores": [
+          3,
+          4,
+          5,
+          4,
+          3,
+          4,
+          5,
+          4,
+          3,
+          1
+        ]
+      }
+    ]
  
